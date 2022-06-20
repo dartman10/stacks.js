c668654eb91a18d2758f9695ddf49791b9aaec7d
Peter Nash <34408740+dartman10@users.noreply.github.com>
2022-06-20 13:01:53 -0400

    update

--- punks-app/src/packages/wallet-sdk/webpack.config.js
+++
@@ -1,9 +0,0 @@
-const config = require('../../configs/webpack.config.js');
-
-config.output.library.name = 'StacksWalletSdk';
-
-config.resolve.fallback = {
-  util: require.resolve('util/'),
-};
-
-module.exports = config;
